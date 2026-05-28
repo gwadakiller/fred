cd533b463c4233a9a636119ce08163e0af7f97a9
gwadakiller <frederic.sagenly@gmail.com>
2026-05-28 15:15:19 +0200

    fix sidebar structure

--- sidebars.js
+++ sidebars.js
@@ -1,29 +1,8 @@
 module.exports = {
   tutorialSidebar: [
-    'intro',
     'installation',
-    {
-      type: 'category',
-      label: 'Backend',
-      collapsible: true,
-      collapsed: true,
-
-      items: [
-        'backend/server',
-        'backend/api',
-      ],
-    },
-
-    {
-      type: 'category',
-      label: 'Frontend',
-      collapsible: true,
-      collapsed: true,
-
-      items: [
-        'frontend/ui',
-        'frontend/components',
-      ],
-    },
+    'backend',
+    'frontend',
+    'intro',
   ],
 };
