8137fd4dd7f98e98412850c85ec75cfe463ac320
gwadakiller <frederic.sagenly@gmail.com>
2026-06-28 18:13:13 +0200

    Premier commit

---
+++ docs/frontend.md
@@ -0,0 +1,8 @@
+# Présentation service radar
+
+## 4.1 Rôle du service
+                                                                                                                                Le service Radar assure le maintien en condition opérationnelle des systèmes de surveillance aérienne utilisés pour la détection et le suivi des aéronefs.
+
+Ces systèmes permettent aux contrôleurs aériens d’obtenir en temps réel les informations nécessaires à la gestion du trafic aérien.
+
+Les radaristes interviennent aussi bien sur des équipements matériels que sur des systèmes logiciels complexes.
--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,34 @@
 module.exports = {
   tutorialSidebar: [
-    'installation',
-    'backend',
-    'frontend',
+    // Les pages simples (sans sous-section)
     'intro',
+    'installation',
+    
+    // Une grande section "Développement"
+    {
+      type: 'category',
+      label: 'Développement',
+      collapsed: false, // false = la section est ouverte par défaut
+      items: [
+        // Sous-section Frontend
+        {
+          type: 'category',
+          label: 'Frontend',
+          items: [
+            'frontend', // Ton fichier actuel
+            // Tu pourras ajouter 'frontend/react', 'frontend/css' plus tard ici
+          ],
+        },
+        // Sous-section init
+        {
+          type: 'category',
+          label: 'initiation au Markdown',
+          items: [
+            'backend',
+            'server',
+          ],
+        },
+      ],
+    },
   ],
 };
